49845f9770f7fb1ad4e10f0097cead068d33e97a
CBecke <christian.becke@outlook.dk>
2025-04-01 14:43:00 +0200

    organizations added

---
+++ orgB/src/main/java/com/example/orgb/OrgBApplication.java
@@ -0,0 +1,13 @@
+package com.example.orgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgBApplication.class, args);
+    }
+
+}
---
+++ orgB/src/test/java/com/example/orgb/OrgBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
