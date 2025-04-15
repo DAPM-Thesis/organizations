92bdd8df98345436cae9caa79d340127a36d8c8d
raihanmehran <raihan.mehran@hotmail.com>
2025-04-15 15:55:49 +0200

    ACPE implemented

--- orgA/src/main/java/com/example/demo/OrgAApplication.java
+++ orgA/src/main/java/com/example/demo/OrgAApplication.java
@@ -1,17 +1,24 @@
 package com.example.demo;
 
+import impl.pipe1.MyEventAlgorithm;
+import impl.pipe1.MyEventOperator;
+import impl.pipe1.MyEventSource;
+import impl.pipe1.MySink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import pipeline.Pipeline;
 import pipeline.PipelineBuilder;
+import pipeline.accesscontrolled.processingelement.AccessControlledProcessingElement;
+import pipeline.accesscontrolled.processingelement.ProcessingElementToken;
+import pipeline.processingelement.ProcessingElement;
 import pipeline.processingelement.ProcessingElementReference;
 import pipeline.processingelement.ProcessingElementType;
 import pipeline.service.PipelineExecutionService;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"controller", "pipeline", "communication"})
+@ComponentScan(basePackages = {"controller", "pipeline", "communication", "impl.pipe1"})
 public class OrgAApplication {
 
     public static void main(String[] args) {
@@ -20,6 +27,7 @@ public class OrgAApplication {
         String orgAID = "orgA";
         String orgBID = "orgB";
 
+        /*
         // Currently the processElementID's don't matter
         ProcessingElementReference sourceRef = new ProcessingElementReference(orgAID, 1, ProcessingElementType.SOURCE);
         ProcessingElementReference operatorRef = new ProcessingElementReference(orgBID, 2, ProcessingElementType.OPERATOR);
@@ -32,6 +40,25 @@ public class OrgAApplication {
                 .addProcessingElement(sinkRef)
                 .connect(sourceRef, operatorRef)
                 .connect(operatorRef, sinkRef)
+                .getCurrentPipeline();
+
+         */
+
+        var sourceToken = new ProcessingElementToken("sourceTokenValue", orgAID);
+        var operatorToken = new ProcessingElementToken("operatorTokenValue", orgBID);
+        var sinkToken = new ProcessingElementToken("sinkTokenValue", orgAID);
+
+        var sourceACPE = new AccessControlledProcessingElement<ProcessingElement>(new MyEventSource(), sourceToken);
+        var operatorACPE = new AccessControlledProcessingElement<ProcessingElement>(new MyEventOperator(new MyEventAlgorithm()), operatorToken);
+        var sinkACPE = new AccessControlledProcessingElement<ProcessingElement>(new MySink(), sinkToken);
+
+        PipelineBuilder builder = context.getBean(PipelineBuilder.class);
+        Pipeline pipeline = builder.createPipeline(orgAID)
+                .addACPE(sourceACPE)
+                .addACPE(operatorACPE)
+                .addACPE(sinkACPE)
+                .connec(sourceACPE, operatorACPE)
+                .connec(operatorACPE, sinkACPE)
                 .getCurrentPipeline();
 
         PipelineExecutionService executionService = context.getBean(PipelineExecutionService.class);
