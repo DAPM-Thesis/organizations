dc670e226ef1d00a4c520b48fc423275574618eb
Yonna <yonnaklaassen@gmail.com>
2025-04-23 15:16:25 +0200

    Pipelinebuilder rework

--- orgB/src/main/java/controller/PipelineBuilderController.java
+++ orgB/src/main/java/controller/PipelineBuilderController.java
@@ -1,14 +1,11 @@
 package controller;
 
 import communication.API.PEInstanceResponse;
+import communication.config.ChannelConfig;
 import communication.message.Message;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pipeline.processingelement.InstanceMetaData;
+import org.springframework.web.bind.annotation.*;
 import pipeline.processingelement.Sink;
 import pipeline.processingelement.Source;
 import pipeline.processingelement.operator.Operator;
@@ -19,6 +16,8 @@ import utils.JsonUtil;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -30,103 +29,61 @@ public class PipelineBuilderController {
     private final TemplateRepository templateRepository = new TemplateRepository();
     private final PEInstanceRepository peInstanceRepository = new PEInstanceRepository();
 
-    @PostMapping("/source/templateID/{templateID}/instanceNumber/{instanceNumber}")
-    public ResponseEntity<PEInstanceResponse> configureSource(@PathVariable String templateID, @PathVariable int instanceNumber) {
+    @PostMapping("/source/templateID/{templateID}")
+    public ResponseEntity<PEInstanceResponse> configureSource(@PathVariable String templateID) {
         String decodedTemplateID = JsonUtil.decode(templateID);
         String topic = IDGenerator.generateTopic();
-        String instanceMetaDataID = peInstanceRepository.storeInstanceMetaData(decodedTemplateID, instanceNumber, orgBBroker, topic, true);
 
         Source<Message> source = templateRepository.createInstanceFromTemplate(decodedTemplateID);
         if (source != null) {
             source.registerProducer(orgBBroker, topic);
-            String instanceID = peInstanceRepository.storeInstance(source, new String[]{instanceMetaDataID});
+            String instanceID = peInstanceRepository.storeInstance(source);
 
-            return ResponseEntity.ok(new PEInstanceResponse.Builder(decodedTemplateID, instanceNumber)
-                    .broker(orgBBroker)
-                    .topic(topic)
-                    .instanceID(instanceID)
+            return ResponseEntity.ok(new PEInstanceResponse
+                    .Builder(decodedTemplateID, instanceID)
+                    .channelConfig(new ChannelConfig(orgBBroker, topic))
                     .build());
         }
         return ResponseEntity.badRequest().body(null);
     }
 
-    @PostMapping("/operator/consumer/templateID/{templateID}/instanceNumber/{instanceNumber}/broker/{broker}/topic/{topic}")
-    public ResponseEntity<PEInstanceResponse> storeOperatorConsumer(@PathVariable String templateID, @PathVariable int instanceNumber, @PathVariable String broker, @PathVariable String topic) {
-        String decodedTemplateID = JsonUtil.decode(templateID);
-        String decodedTopic = JsonUtil.decode(topic);
-        String decodedBroker = JsonUtil.decode(broker);
-        String instanceMetaDataID = peInstanceRepository.storeInstanceMetaData(decodedTemplateID, instanceNumber, decodedBroker, decodedTopic, false);
-
-        return ResponseEntity.ok(new PEInstanceResponse.Builder(decodedTemplateID, instanceNumber)
-                .instanceMetaDataID(instanceMetaDataID)
-                .build());
-    }
-
-    @PostMapping("/operator/producer/templateID/{templateID}/instanceNumber/{instanceNumber}")
-    public ResponseEntity<PEInstanceResponse> storeOperatorProducer(@PathVariable String templateID, @PathVariable int instanceNumber) {
-        String decodedTemplateID = JsonUtil.decode(templateID);
-        String topic = IDGenerator.generateTopic();
-        String instanceMetaDataID = peInstanceRepository.storeInstanceMetaData(decodedTemplateID, instanceNumber, orgBBroker, topic, true);
-
-        return ResponseEntity.ok(new PEInstanceResponse.Builder(decodedTemplateID, instanceNumber)
-                .broker(orgBBroker)
-                .topic(topic)
-                .instanceMetaDataID(instanceMetaDataID)
-                .build());
-    }
-
-    @PostMapping("/operator/templateID/{templateID}/instance/{instanceMetaDataIDS}")
-    public ResponseEntity<Void> createOperator(@PathVariable String templateID, @PathVariable("instanceMetaDataIDS") String instanceMetaDataIDS) {
+    @PostMapping("/operator/templateID/{templateID}")
+    public ResponseEntity<PEInstanceResponse> createOperator(@PathVariable String templateID, @RequestBody List<ChannelConfig> channelConfigs) {
         String decodedTemplateID = JsonUtil.decode(templateID);
         Operator<Message, Message> operator = templateRepository.createInstanceFromTemplate(decodedTemplateID);
         if (operator != null) {
-            String decodedInstanceMetaDataIDS = JsonUtil.decode(instanceMetaDataIDS);
-            String[] instanceMetaDataIDList = decodedInstanceMetaDataIDS.split(",");
-            for (String instanceID : instanceMetaDataIDList) {
-                InstanceMetaData metadata = peInstanceRepository.getInstanceMetaData(instanceID);
-                if (metadata != null) {
-                    if (!metadata.isProducer()) operator.registerConsumer(metadata.brokerURL(), metadata.topic());
-                    else operator.registerProducer(metadata.brokerURL(), metadata.topic());
-                }
+            for (ChannelConfig config : channelConfigs) {
+                operator.registerConsumer(config.brokerURL(), config.topic());
             }
-            peInstanceRepository.storeInstance(operator, instanceMetaDataIDList);
-            return ResponseEntity.ok().build();
+            String topic = IDGenerator.generateTopic();
+            operator.registerProducer(orgBBroker, topic);
+
+            String instanceID = peInstanceRepository.storeInstance(operator);
+            return ResponseEntity.ok(new PEInstanceResponse
+                    .Builder(decodedTemplateID, instanceID)
+                    .channelConfig(new ChannelConfig(orgBBroker, topic))
+                    .build());
         }
         return ResponseEntity.badRequest().body(null);
     }
 
-    @PostMapping("/sink/templateID/{templateID}/instanceNumber/{instanceNumber}/broker/{broker}/topic/{topic}")
-    public ResponseEntity<PEInstanceResponse> storeSinkConsumer(@PathVariable String templateID, @PathVariable int instanceNumber, @PathVariable String broker, @PathVariable String topic) {
-        String decodedTemplateID = JsonUtil.decode(templateID);
-        String decodedTopic = JsonUtil.decode(topic);
-        String decodedBroker = JsonUtil.decode(broker);
-        String instanceMetaDataID = peInstanceRepository.storeInstanceMetaData(decodedTemplateID, instanceNumber, decodedBroker, decodedTopic, false);
-
-        return ResponseEntity.ok(new PEInstanceResponse.Builder(decodedTemplateID, instanceNumber)
-                .instanceMetaDataID(instanceMetaDataID)
-                .build());
-    }
-
-    @PostMapping("/sink/templateID/{templateID}/instance/{instanceMetaDataIDS}")
-    public ResponseEntity<Void> createSink(@PathVariable String templateID, @PathVariable("instanceMetaDataIDS") String instanceMetaDataIDS) {
+    @PostMapping("/sink/templateID/{templateID}")
+    public ResponseEntity<PEInstanceResponse> createSink(@PathVariable String templateID, @RequestBody List<ChannelConfig> channelConfigs) {
         String decodedTemplateID = JsonUtil.decode(templateID);
         Sink sink = templateRepository.createInstanceFromTemplate(decodedTemplateID);
         if (sink != null) {
-            String decodedInstanceMetaDataIDS = JsonUtil.decode(instanceMetaDataIDS);
-            String[] instanceMetaDataIDList = decodedInstanceMetaDataIDS.split(",");
-            for (String instanceID : instanceMetaDataIDList) {
-                InstanceMetaData metadata = peInstanceRepository.getInstanceMetaData(instanceID);
-                if (metadata != null) {
-                    sink.registerConsumer(metadata.brokerURL(), metadata.topic());
-                }
+            for (ChannelConfig config : channelConfigs) {
+                sink.registerConsumer(config.brokerURL(), config.topic());
             }
-            peInstanceRepository.storeInstance(sink, instanceMetaDataIDList);
-            return ResponseEntity.ok().build();
+            String instanceID = peInstanceRepository.storeInstance(sink);
+            return ResponseEntity.ok(new PEInstanceResponse
+                    .Builder(decodedTemplateID, instanceID)
+                    .build());
         }
         return ResponseEntity.badRequest().body(null);
     }
 
-    @PostMapping("/start/instance/{instanceID}")
+    @PutMapping("/start/instance/{instanceID}")
     public ResponseEntity<Void> startSource(@PathVariable String instanceID) {
         Source<Message> source = peInstanceRepository.getInstance(instanceID);
         if (source != null) {
--- orgB/src/main/java/repository/PEInstanceRepository.java
+++ orgB/src/main/java/repository/PEInstanceRepository.java
@@ -1,7 +1,6 @@
 package repository;
 
 import org.springframework.stereotype.Repository;
-import pipeline.processingelement.InstanceMetaData;
 import pipeline.processingelement.ProcessingElement;
 import utils.IDGenerator;
 
@@ -10,49 +9,15 @@ import java.util.*;
 @Repository
 public class PEInstanceRepository {
 
-    public Map<String, InstanceMetaData> instanceMetaData;
     public Map<String, ProcessingElement> instances;
 
     public PEInstanceRepository() {
-        instanceMetaData = new HashMap<>();
         instances = new HashMap<>();
     }
 
-    public String storeInstanceMetaData(String templateID, int instanceNumber, String brokerURL, String topic, boolean isProducer) {
-        String instanceMetaDataID = IDGenerator.generateInstanceMetaDataID();
-        instanceMetaData.put(instanceMetaDataID, new InstanceMetaData(
-                instanceMetaDataID,
-                templateID,
-                instanceNumber,
-                brokerURL,
-                topic,
-                isProducer,
-                null));
-        return instanceMetaDataID;
-    }
-
-    public InstanceMetaData getInstanceMetaData(String instanceMetaDataID) {
-        return instanceMetaData.get(instanceMetaDataID);
-    }
-
-    public String storeInstance(ProcessingElement instance, String[] instanceMetaDataIDS) {
+    public String storeInstance(ProcessingElement instance) {
         String instanceID = IDGenerator.generateInstanceID();
         instances.put(instanceID, instance);
-        for (String instanceMetaData : instanceMetaDataIDS) {
-            InstanceMetaData metadata = this.instanceMetaData.get(instanceMetaData);
-            if (metadata != null && metadata.instanceID() == null) {
-                InstanceMetaData updated = new InstanceMetaData(
-                        metadata.instanceDetailID(),
-                        metadata.templateID(),
-                        metadata.instanceNumber(),
-                        metadata.brokerURL(),
-                        metadata.topic(),
-                        metadata.isProducer(),
-                        instanceID
-                );
-                this.instanceMetaData.put(instanceMetaData, updated);
-            }
-        }
         return instanceID;
     }
 
